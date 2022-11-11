53daf269543c641edcfa283814b3b230ea56e1cf
qunnderrie1 <qunnderrie@gmail.com>
2022-11-10 21:16:51 -0500

    first changes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import FilmsList from './Components/FilmsList.jsx';
+import Header from '../src/Components/Header.jsx';
+import { useState } from 'react';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Header />
+      <FilmsList />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () =>
+{
+  return (
+    <div className='HeaderContainer container-fluid shadow-md' >
+        <h1>Films</h1>
+    </div>
+  )
+}
+
+
+export default Header;
